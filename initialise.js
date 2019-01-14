1aa981146a229843728a924d5eaf038b4dcbb2ef
Germain SIPIERE <gsipiere@gmail.com>
2019-01-14 10:55:44 +0100

    update

---
+++ initialise.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".sidenav").sidenav();
+    $(".collapsible").collapsible();
+    $(".materialboxed").materialbox();
+  });
+  $(".dropdown-trigger").dropdown();
